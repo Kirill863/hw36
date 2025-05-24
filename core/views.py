b315e741ac95a1b6007de3374bc772d666edee78
Kirill863 <Byblik4310@gmail.com>
2025-05-24 21:13:55 +0300

    hw36 обновил пути

--- core/views.py
+++ core/views.py
@@ -1,18 +1,41 @@
-from django.shortcuts import render
+
 
 # Create your views here.
 
 
+from django.shortcuts import render
+from .data import masters, services, orders, STATUS_NEW, STATUS_CONFIRMED, STATUS_CANCELLED, STATUS_COMPLETED
+
+
 def landing(request):
-    return render(request, 'landing.html')
+    return render(request, 'landing.html', {
+        'masters': masters,
+        'services': services,
+    })
+
 
 def thanks(request):
     return render(request, 'core/thanks.html')
 
+
 def orders_list(request):
-    
-    return render(request, 'core/orders_list.html')
+    return render(request, 'core/orders_list.html', {
+        'orders': orders,
+        'statuses': {
+            'новая': STATUS_NEW,
+            'подтвержденная': STATUS_CONFIRMED,
+            'отмененная': STATUS_CANCELLED,
+            'выполненная': STATUS_COMPLETED
+        }
+    })
+
 
 def order_detail(request, order_id):
-    
-    return render(request, 'core/order_detail.html')
+    order = next((o for o in orders if o['id'] == order_id), None)
+    if not order:
+        # Можно добавить обработку ошибки 404
+        return render(request, 'core/order_detail.html', {'order': None})
+    return render(request, 'core/order_detail.html', {
+        'order': order,
+        'masters': masters
+    })
