08be01cfca1528a6c6832cfff07e341fdb8061f4
Kirill863 <Byblik4310@gmail.com>
2025-05-24 20:47:47 +0300

    hw36: добавил функцию рендер

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def landing(request):
+    return render(request, 'landing.html')
+
+def thanks(request):
+    return render(request, 'core/thanks.html')
+
+def orders_list(request):
+    
+    return render(request, 'core/orders_list.html')
+
+def order_detail(request, order_id):
+    
+    return render(request, 'core/order_detail.html')
