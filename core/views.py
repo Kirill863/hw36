ba3a4412491e2892bfd742e97b755b34543cedf8
Kirill863 <Byblik4310@gmail.com>
2025-06-04 13:31:50 +0300

    hw22_исправил ошибки

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@
 
 from django.shortcuts import render
 from .data import masters, services, orders, STATUS_NEW, STATUS_CONFIRMED, STATUS_CANCELLED, STATUS_COMPLETED
-
+from django.contrib.admin.views.decorators import staff_member_required
 
 def landing(request):
     return render(request, 'landing.html', {
@@ -17,7 +17,7 @@ def landing(request):
 def thanks(request):
     return render(request, 'core/thanks.html')
 
-
+@staff_member_required
 def orders_list(request):
     return render(request, 'core/orders_list.html', {
         'orders': orders,
