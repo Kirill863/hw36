63d77e17771ebe82fb88747f14520471b3c7587a
Kirill863 <Byblik4310@gmail.com>
2025-06-16 16:52:41 +0300

    hw23_добавил модель услуги

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Service(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    duration = models.PositiveIntegerField(verbose_name="Длительность (минуты)")
+    is_popular = models.BooleanField(default=False, verbose_name="Популярная услуга")
+    image = models.ImageField(upload_to='services/', blank=True, verbose_name="Изображение")
+
+    def __str__(self):
+        return self.name
