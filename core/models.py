438366348ef43ee73e807985f78df035ea6123ee
Kirill863 <Byblik4310@gmail.com>
2025-06-24 22:39:09 +0300

    hw24_добавил астеров

--- core/models.py
+++ core/models.py
@@ -21,7 +21,7 @@ class Master(models.Model):
     photo = models.ImageField(upload_to='masters/', blank=True, verbose_name="Фотография")
     phone = models.CharField(max_length=20, verbose_name="Телефон")
     address = models.CharField(max_length=255, verbose_name="Адрес")
-    experience = models.PositiveIntegerField(verbose_name="Стаж работы (лет)")
+    experience = models.PositiveIntegerField(null=True, blank=True) 
     services = models.ManyToManyField(Service, related_name='masters', verbose_name="Услуги")
     is_active = models.BooleanField(default=True, verbose_name="Активен")
     
