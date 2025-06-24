3bd063f2c5435dccc249f32dd8564d3df75a46b4
Kirill863 <Byblik4310@gmail.com>
2025-06-24 16:10:12 +0300

    hw24_обновил views

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
-# views.py
-
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Master, Service, Order, Review
+
+
+def landing(request):
+    return render(request, 'barbershop/landing.html')
+
 
 def index(request):
     masters = Master.objects.all()
