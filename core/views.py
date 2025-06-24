8b8e86ff033cd91c6f4d901ba695d45526f3f270
Kirill863 <Byblik4310@gmail.com>
2025-06-24 21:10:08 +0300

    hw24_изменил структуру проекта

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Master, Service, Order, Review
-
+from .data import services
 
 
 def index(request):
