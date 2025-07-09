a04f7f90a0572b4d6f2c784ff640b966b8c33cfe
Kirill863 <Byblik4310@gmail.com>
2025-07-09 19:58:10 +0300

    hw26_установил бибилиотеку mistralai

--- barbershop/wsgi.py
+++ barbershop/wsgi.py
@@ -7,7 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+
 import os
+from dotenv import load_dotenv
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+load_dotenv()
 
 from django.core.wsgi import get_wsgi_application
 
