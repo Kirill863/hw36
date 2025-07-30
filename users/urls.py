9382eebedf0288a2f5216f4e57ce7f24b6310349
Kirill863 <Byblik4310@gmail.com>
2025-07-30 17:20:55 +0300

    hw31_исправил userr/urls

--- users/urls.py
+++ users/urls.py
@@ -1,53 +1,54 @@
-from django.urls import path
-from django.contrib.auth.views import (
-    LoginView, LogoutView, 
-    PasswordChangeView, PasswordChangeDoneView,
-    PasswordResetView, PasswordResetDoneView,
-    PasswordResetConfirmView, PasswordResetCompleteView
-)
-from .views import RegisterView, ProfileDetailView, ProfileUpdateView
-
-app_name = 'users'
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(
-        template_name='users/login.html',
-        redirect_authenticated_user=True,
-        extra_context={'title': 'Авторизация'}
-    ), name='login'),
-    path('logout/', LogoutView.as_view(
-        template_name='users/logout.html',
-        next_page='/'
-    ), name='logout'),
+    # Регистрация и аутентификация
+    path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
     
-    # Профиль
-    path('profile/', ProfileDetailView.as_view(), name='profile_detail'),
-    path('profile/edit/', ProfileUpdateView.as_view(), name='profile_edit'),
+    # Профиль пользователя
+    path('profile/<int:pk>/', views.UserProfileDetailView.as_view(), name='profile_detail'),
+    path('profile/edit/', views.UserProfileUpdateView.as_view(), name='profile_edit'),
     
     # Смена пароля
-    path('password-change/', PasswordChangeView.as_view(
-        template_name='users/password_change_form.html',
-        success_url='/users/password-change/done/'
-    ), name='password_change'),
-    path('password-change/done/', PasswordChangeDoneView.as_view(
-        template_name='users/password_change_done.html'
-    ), name='password_change_done'),
+    path('password-change/', views.UserPasswordChangeView.as_view(), name='password_change'),
     
     # Восстановление пароля
-    path('password-reset/', PasswordResetView.as_view(
-        template_name='users/password_reset_form.html',
-        email_template_name='users/password_reset_email.html',
-        success_url='/users/password-reset/done/'
-    ), name='password_reset'),
-    path('password-reset/done/', PasswordResetDoneView.as_view(
-        template_name='users/password_reset_done.html'
-    ), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
-        template_name='users/password_reset_confirm.html',
-        success_url='/users/reset/done/'
-    ), name='password_reset_confirm'),
-    path('reset/done/', PasswordResetCompleteView.as_view(
-        template_name='users/password_reset_complete.html'
-    ), name='password_reset_complete'),
+    path('password-reset/', 
+        views.CustomPasswordResetView.as_view(
+            template_name='users/password_reset_form.html',
+            email_template_name='users/password_reset_email.html',
+            subject_template_name='users/password_reset_subject.txt'
+        ), 
+        name='password_reset'),
+    
+    path('password-reset/done/', 
+        views.CustomPasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ), 
+        name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', 
+        views.CustomPasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ), 
+        name='password_reset_confirm'),
+    
+    path('reset/done/', 
+        views.CustomPasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ), 
+        name='password_reset_complete'),
+    
+    # Включение URL-адресов аутентификации по умолчанию (резервный вариант)
+    # path('', include('django.contrib.auth.urls')),
 ]
+
+# Настройка для медиа-файлов в режиме разработки
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
